d409088a6d06fb26c9b5d0d9009be5524c356ad3
hoseok <hs1j333@gmail.com>
2025-07-20 13:22:47 +0900

    users domain model 구현

---
+++ src/users/model/tag/user.tag.base.ts
@@ -0,0 +1,15 @@
+import { ImmutableModelBase } from "../../../common";
+import { Column, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "../user.model";
+
+export class UserTagBase extends ImmutableModelBase {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ name: "user_id", type: "integer" })
+    userId: number;
+
+    @ManyToOne(() => User, { onDelete: "CASCADE" })
+    @JoinColumn({ name: "user_id" })
+    user: User;
+}
