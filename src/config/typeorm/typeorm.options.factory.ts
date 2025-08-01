82400908f29a4978ae981660ec0daf617ff51bc6
hoseok <hs1j333@gmail.com>
2025-08-02 08:49:58 +0900

    배포 스크립트 포함

--- src/config/typeorm/typeorm.options.factory.ts
+++ src/config/typeorm/typeorm.options.factory.ts
@@ -1,7 +1,15 @@
 import { ConfigService } from "@nestjs/config";
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import * as fs from "node:fs";
 
 export function typeormOptionsFactory(config: ConfigService): TypeOrmModuleOptions {
+    const ca = config.get<string>("DB_SSL_CA");
+
+    const ssl = ca ? {
+                ca: fs.readFileSync(ca).toString(),
+                rejectUnauthorized: true,
+            } : false;
+
     return {
         type: config.get<any>("DB_TYPE"),
         host: config.get<string>("DB_HOST")!,
@@ -10,6 +18,8 @@ export function typeormOptionsFactory(config: ConfigService): TypeOrmModuleOptio
         password: config.get<string>("DB_PASSWORD")!,
         database: config.get<string>("DB_DATABASE")!,
         entities: [__dirname + '/../../**/*.model{.ts,.js}'],
+        ssl,
         logging: false
     };
 }
+
