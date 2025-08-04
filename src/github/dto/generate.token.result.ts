c66318f8ff78ec3cde6a2c29fb215e274b1d541e
hoseok <hs1j333@gmail.com>
2025-08-04 14:24:51 +0900

    github 로그인 rxjs 구현 모두 제거, promise로 교체

--- src/github/dto/generate.token.result.ts
+++ src/github/dto/generate.token.result.ts
@@ -3,7 +3,4 @@ import { Expose } from "class-transformer";
 export class GenerateTokenResult {
     @Expose({ name: "access_token" })
     accessToken: string;
-
-    @Expose({ name: "refresh_token" })
-    refreshToken: string;
 }
--- src/github/service/get.user.service.ts
+++ src/github/service/get.user.service.ts
@@ -1,35 +1,38 @@
-import { Inject, Injectable } from "@nestjs/common";
+import { Inject, Injectable, Logger } from "@nestjs/common";
 import { HttpService } from "@nestjs/axios";
-import { from, map, mergeMap, Observable } from "rxjs";
 import { GithubUserDTO } from "../dto";
 import { plainToInstanceOrReject } from "../../utils";
+import { AxiosResponse } from "axios";
 
 const __USER_URL = "https://api.github.com/user";
 
 @Injectable()
 export class GetUserService {
+    private readonly _logger: Logger = new Logger(GetUserService.name);
 
     constructor(
        @Inject(HttpService)
        private readonly _httpService: HttpService,
     ) {}
 
-    getUser(accessToken: string): Observable<GithubUserDTO> {
-        return this._httpService.get(
+    async getUser(accessToken: string): Promise<GithubUserDTO> {
+        const { data } = await this.sendRequest(accessToken);
+        this._logger.debug(data);
+
+        return plainToInstanceOrReject(
+            GithubUserDTO,
+            data,
+            {
+                transform: { excludeExtraneousValues: true },
+                validate: { forbidUnknownValues: false }
+            }
+        );
+    }
+
+    private sendRequest(accessToken: string): Promise<AxiosResponse> {
+        return this._httpService.axiosRef.get(
             __USER_URL,
             { headers: { Authorization: `Bearer ${accessToken}` } }
-        ).pipe(
-            map(({ data }) => data),
-            mergeMap(data =>
-                from(plainToInstanceOrReject(
-                    GithubUserDTO,
-                    data,
-                    {
-                        transform: { excludeExtraneousValues: true },
-                        validate: { forbidUnknownValues: false }
-                    }
-                ))
-            )
         );
     }
 }
