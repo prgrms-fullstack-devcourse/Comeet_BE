0e08a8e732c50965f8941bbd006c1b2ce4c5b27a
hoseok <hs1j333@gmail.com>
2025-08-04 17:53:04 +0900

    github 로그인 rxjs 구현 모두 제거, promise로 교체

--- src/auth/api/sign.in.response.ts
+++ src/auth/api/sign.in.response.ts
@@ -1,6 +1,9 @@
 import { ApiProperty } from "@nestjs/swagger";
 
 export class SignInResponse {
+    @ApiProperty({ type: "boolean" })
+    success: boolean;
+
     @ApiProperty({ type: "string" })
     accessToken: string;
 }
