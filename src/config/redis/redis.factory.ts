fe137425d9df6c6a79f98773356b0ed24be891d0
hoseok <hs1j333@gmail.com>
2025-08-04 20:36:53 +0900

    fix

--- src/config/redis/redis.factory.ts
+++ src/config/redis/redis.factory.ts
@@ -1,13 +1,17 @@
 import { ConfigService } from "@nestjs/config";
 import Redis from "iovalkey";
+import { readFileSync } from "node:fs";
+
 
 export async function redisFactory(config: ConfigService): Promise<Redis> {
+    const ca = config.get<string>("REDIS_TLS_CA");
+    const tls = ca ? { ca: readFileSync(ca) } : undefined;
+
     return new Redis({
         host: config.get<string>("REDIS_HOST")!,
         port: config.get<number>("REDIS_PORT")!,
         db: config.get<number>("REDIS_DB") ?? 0,
-        tls: config.get("NODE_ENV") === "production"
-            ? {} : undefined
+        tls
     });
 }
 
