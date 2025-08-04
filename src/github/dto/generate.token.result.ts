875702f3466f76a3ad9eece39dd43cb34218efdb
hoseok <hs1j333@gmail.com>
2025-08-04 14:56:54 +0900

    github 로그인 rxjs 구현 모두 제거, promise로 교체

--- src/github/dto/generate.token.result.ts
+++ src/github/dto/generate.token.result.ts
@@ -1,6 +1,9 @@
 import { Expose } from "class-transformer";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class GenerateTokenResult {
+    @IsString()
+    @IsNotEmpty()
     @Expose({ name: "access_token" })
     accessToken: string;
 }
