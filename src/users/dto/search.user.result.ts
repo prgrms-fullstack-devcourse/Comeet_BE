248d12c7acc3939767b9c5c80eb256a5f477f283
hoseok <hs1j333@gmail.com>
2025-08-06 16:30:56 +0900

    fix

--- src/users/dto/search.user.result.ts
+++ src/users/dto/search.user.result.ts
@@ -3,5 +3,5 @@ import { UserDTO } from "./user.dto";
 
 export class SearchUserResult extends PickType(
     UserDTO,
-    ["nickname", "birthyear", "experience", "position", "techStack", "interests", "nSubscribers", "location"]
+    ["nickname", "avatar", "birthyear", "experience", "position", "techStack", "interests", "nSubscribers", "location"]
 ) {}
