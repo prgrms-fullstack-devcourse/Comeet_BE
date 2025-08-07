938256a5253f337638f226c5163b1f588b3f1248
hoseok <hs1j333@gmail.com>
2025-08-07 13:28:24 +0900

    쿼리 파라마터 정수 변환 코드 추가

--- src/posts/dto/post/search.posts.dto.ts
+++ src/posts/dto/post/search.posts.dto.ts
@@ -1,18 +1,21 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsInt, IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { Transform } from "class-transformer";
 
 export class SearchPostsDTO {
     userId?: number;
 
-    @IsInt()
     @IsOptional()
-    @ApiProperty({ type: "integer", required: false })
+    @IsInt()
+    @Transform(({ value }) =>
+        typeof value === 'undefined' ? value : parseInt(value, 10),
+    )
+    @ApiProperty({ type: "integer", required: false, description: "조회할 게시판 id" })
     boardId?: number;
 
-
+    @IsOptional()
     @IsString()
     @IsNotEmpty()
-    @IsOptional()
     @ApiProperty({ type: "string", required: false })
     keyword?: string;
 }
